d9f12f1a627678645c577d3cd55af6114a0c5622
Jonathan Lurie <lurie.jo@gmail.com>
2024-12-13 09:31:33 +0100

    Adding telemetry (#144)
    * Adding telemetry
    
    * update telemetry endpoint
    
    * now using a Set

--- src/defaults.ts
+++ src/defaults.ts
@@ -7,6 +7,7 @@ const defaults = {
   maptilerLogoURL: "https://api.maptiler.com/resources/logo.svg",
   maptilerURL: "https://www.maptiler.com/",
   maptilerApiHost: "api.maptiler.com",
+  telemetryURL: "https://api.maptiler.com/metrics",
   rtlPluginURL: "https://cdn.maptiler.com/mapbox-gl-rtl-text/v0.2.3/mapbox-gl-rtl-text.min.js",
   primaryLanguage: Language.STYLE,
   secondaryLanguage: Language.LOCAL,
