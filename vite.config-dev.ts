4a1d714fea14b57f1301d9dba2cdd4cdb5cf821c
Sebastian <sebastian@stryczek.pl>
2025-01-06 14:08:59 +0100

    Split vite config files

--- vite.config-dev.ts
+++ vite.config-dev.ts
@@ -1,12 +1,6 @@
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-  test: {
-    include: ["test/**/*.test.ts"],
-    typecheck: {
-      tsconfig: "./tsconfig.json",
-    },
-  },
   plugins: [
     {
       name: 'url-override',
