cf628f07d0de761e000b01eea61f62b522761f4b
Sebastian <sebastian@stryczek.pl>
2025-01-06 10:55:11 +0100

    Make biome happy

--- src/index.ts
+++ src/index.ts
@@ -262,7 +262,7 @@ export {
 export { getWebGLSupportError } from "./tools";
 export { config, SdkConfig } from "./config";
 export * from "./language";
-export { type Unit } from "./unit";
+export type { Unit } from "./unit";
 export * from "./Minimap";
 export * from "./converters";
 export * from "./colorramp";
