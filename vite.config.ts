75628c0e4bf026a194ef398ac2ab412aa8f6a2d8
Sebastian <sebastian@stryczek.pl>
2025-01-06 10:51:46 +0100

    Update demos

--- vite.config.ts
+++ vite.config.ts
@@ -7,4 +7,24 @@ export default defineConfig({
       tsconfig: "./tsconfig.json",
     },
   },
+  plugins: [
+    {
+      name: 'url-override',
+      configureServer: (server) => {
+        const printUrls = server.printUrls;
+
+        server.printUrls = () => {
+          if (server.resolvedUrls !== null) {
+            for (const [key, value] of Object.entries(server.resolvedUrls)) {
+              for (let i = 0; i < value.length; i++) {
+                server.resolvedUrls[key][i] += "demos/index.html";
+              }
+            }
+          }
+
+          printUrls();
+        }
+      }
+    }
+  ],
 });
