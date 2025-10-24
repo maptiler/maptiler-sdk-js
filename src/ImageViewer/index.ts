324956489229d6491d106882b054a6690f05ce38
Les <iamiamiamles@gmail.com>
2025-10-24 15:17:08 +0100

    RD 1284 Add additional methods to ImageViewer (#260)
    * RD-1284 Fix "wrapping" of ImageViewerMarker dom elements
    
    * RD-1284 remove superfluous code
    
    * RD-1284
    - Add additional methods as requested.
    - Add test cases.
    
    ---------
    
    Co-authored-by: Les Moffat <les.moffat@maptiler.com>

--- src/ImageViewer/index.ts
+++ src/ImageViewer/index.ts
@@ -1,5 +1,5 @@
 export { default as ImageViewer } from "./ImageViewer";
-export { ImageViewerMarker } from "./ImageViewerMarker";
+export { ImageViewerMarker, ImageViewerMarkerEvent, type ImageViewerMarkerOptions } from "./ImageViewerMarker";
 export type { ImageViewerEventTypes } from "./events";
 export type { ImageViewerConstructorOptions, ImageViewerFlyToOptions, ImageViewerJumpToOptions, ImageMetadata } from "./ImageViewer";
 export { ImageViewerEvent } from "./events";
