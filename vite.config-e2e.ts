2da86e242dbbd64a93f699dbd28dde3dcf458cc5
Les Moffat <les.moffat@maptiler.com>
2025-11-07 12:20:03 +0000

    RD-1233 update vite and scripts for e2e update action

--- vite.config-e2e.ts
+++ vite.config-e2e.ts
@@ -10,6 +10,12 @@ export default defineConfig({
     },
   },
   root: './e2e',
+  resolve: {
+    alias: {
+      '@maptiler/sdk': '../src',
+      '@maptiler/sdk/dist/maptiler-sdk.css': '../dist/maptiler-sdk.css',
+    },
+  },
   define: {
     __MT_SDK_VERSION__: JSON.stringify(packagejson.version),
     __MT_NODE_ENV__: JSON.stringify(process.env.NODE_ENV),
