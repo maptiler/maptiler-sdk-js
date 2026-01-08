09e8ad58b373d56c1c4c2d26a0ddd26bfbbfebb8
Les Moffat <les.moffat@maptiler.com>
2026-01-08 11:26:14 +0000

    hotfix/add console version message

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,13 @@ export function getVersion(): string {
   return __MT_SDK_VERSION__;
 }
 
+const purpleCSS = "color: #3A1888; background: white; padding: 5px 0; font-weight: bold;";
+const yellowCSS = "color: #FBC935; background: white; padding: 5px 0 5px 5px; font-weight: bold;";
+const redCSS = "color: #F1175D; background: white; padding: 5px 0; font-weight: bold;";
+const blackCSS = "color: #333; background: white; padding: 5px 0; font-weight: bold;";
+
+console.info(`%c❖%c❖%c❖ %cMapTiler SDK JS v${getVersion()} %c❖%c❖%c❖`, yellowCSS, purpleCSS, redCSS, blackCSS, redCSS, purpleCSS, yellowCSS);
+
 const MapMLGL = maplibregl.Map;
 const MarkerMLGL = maplibregl.Marker;
 const PopupMLGL = maplibregl.Popup;
