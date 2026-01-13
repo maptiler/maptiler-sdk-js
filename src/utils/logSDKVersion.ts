a0174a0465208c6f7dda4e39f63c692a55e9dbec
Les Moffat <les.moffat@maptiler.com>
2026-01-13 11:30:55 +0000

    hotfix/fix sdk version loggin

--- src/utils/logSDKVersion.ts
+++ src/utils/logSDKVersion.ts
@@ -2,9 +2,18 @@ import { getVersion } from "..";
 
 export function logSDKVersion() {
   const purpleCSS = "color: #3A1888; background: white; padding: 5px 0; font-weight: bold;";
-  const yellowCSS = "color: #FBC935; background: white; padding: 5px 0 5px 5px; font-weight: bold;";
+  const yellowCSS = "color: #FBC935; background: white; padding: 5px; font-weight: bold;";
   const redCSS = "color: #F1175D; background: white; padding: 5px 0; font-weight: bold;";
   const blackCSS = "color: #333; background: white; padding: 5px 0; font-weight: bold;";
 
-  console.info(`%c❖%c❖%c❖ %cMapTiler SDK JS v${getVersion()} %c❖%c❖%c❖`, yellowCSS, purpleCSS, redCSS, blackCSS, redCSS, purpleCSS, yellowCSS);
+  console.info(
+    `%c❖%c❖%c❖ %cMapTiler SDK JS v${getVersion()} %c❖%c❖%c❖`,
+    yellowCSS + "padding-right: 0;",
+    purpleCSS,
+    redCSS,
+    blackCSS,
+    redCSS,
+    purpleCSS,
+    yellowCSS + "padding-left: 0;",
+  );
 }
