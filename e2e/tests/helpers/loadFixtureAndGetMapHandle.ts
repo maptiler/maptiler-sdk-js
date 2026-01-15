1b9bbeed3b3c0ca663bca15bbc7f5c53faa89a20
Les <iamiamiamles@gmail.com>
2026-01-15 15:14:54 +0000

    RD-1566 rtl is disabled by default (#295)
    * hotfix/rtl-text-plugin-is-not-called
    - ini rtl plugin on load
    
    * hotfix/rtl-text-plugin-is-not-called
    - update changelog
    - remove sarwin snapshots
    
    * RD-1566 Add unit tests to ensure `enableRTL` is fired in constructor on style.load
    
    * [BOT] Update snapshots (Triggered by @lesbaa in PR RD-1566-rtl-is-disabled-by-default)
    
    * RD-1566 remove superfluous comments
    
    ---------
    
    Co-authored-by: MapTiler Testbot [lesbaa] <testbot@maptiler.com>

--- e2e/tests/helpers/loadFixtureAndGetMapHandle.ts
+++ e2e/tests/helpers/loadFixtureAndGetMapHandle.ts
@@ -9,6 +9,7 @@ interface IloadFixtureAndGetMapHandle {
   mockTiles?: boolean;
   debug?: boolean;
   waitUntil?: "load" | "domcontentloaded" | "networkidle";
+  queryParams?: Record<string, string>;
 }
 
 export default async function loadFixtureAndGetMapHandle({
@@ -17,81 +18,82 @@ export default async function loadFixtureAndGetMapHandle({
   mockStyle = true,
   mockTiles = true,
   debug = false,
-  waitUntil = 'load',
+  waitUntil = "load",
+  queryParams,
 }: IloadFixtureAndGetMapHandle): Promise<{ mapHandle: JSHandle<Map | null> }> {
   if (mockStyle) {
     // mock style response
-    await page.route('https://api.maptiler.com/maps/*/*.json*', async route => {
-      if (debug) console.info(`ℹ️ Style intercepted at ${route.request().url()}`)
+    await page.route("https://api.maptiler.com/maps/*/*.json*", async (route) => {
+      if (debug) console.info(`ℹ️ Style intercepted at ${route.request().url()}`);
       await route.fulfill({
         status: 200,
-        contentType: 'application/json',
-        path: path.resolve(import.meta.dirname, '../mocks/maptiler-style.json'),
+        contentType: "application/json",
+        path: path.resolve(import.meta.dirname, "../mocks/maptiler-style.json"),
       });
     });
   }
 
   if (mockTiles) {
     // mocks the tile response always returning the mock tile
-    await page.route('https://api.maptiler.com/tiles/*/*/*/*.jpg?key=*&*', route => {
-      if (debug) console.info(`ℹ️ Tile intercepted at ${route.request().url()}`)
+    await page.route("https://api.maptiler.com/tiles/*/*/*/*.jpg?key=*&*", (route) => {
+      if (debug) console.info(`ℹ️ Tile intercepted at ${route.request().url()}`);
       return route.fulfill({
         status: 200,
-        contentType: 'image/png',
+        contentType: "image/png",
         path: path.resolve(import.meta.dirname, `../mocks/tile.png`),
       });
     });
   }
-  
-  page.on('console', msg => {
-    console.log('FIXTURE LOG:', msg.text());
+
+  page.on("console", (msg) => {
+    console.log("FIXTURE LOG:", msg.text());
     if (debug) {
-      console.log('DEBUG FIXTURE LOG:', msg.location(), msg.text());
+      console.log("DEBUG FIXTURE LOG:", msg.location(), msg.text());
     }
-  })
-  
-  page.addListener('requestfinished', async (request) => {
-    const response = await request.response()
+  });
+
+  page.addListener("requestfinished", async (request) => {
+    const response = await request.response();
     if (response && response.status() >= 400) {
       console.error(`\n\nFailed to load ${request.url()}\n status: ${response.status()}\n\n`);
       expect(response.status()).toBeLessThan(400);
     }
-  })
-  
-  await page.goto(`http://localhost:5173/${fixture}.html`, {
+  });
+
+  await page.goto([`http://localhost:5173/${fixture}.html`, queryParams && new URLSearchParams(queryParams).toString()].filter(Boolean).join("?"), {
     waitUntil,
   });
 
   try {
     const mapHandle = await page.evaluateHandle<Map | null>(() => {
       return Promise.race<Map | null>([
-      new Promise<Map | null>(async (resolve) => {
-        try {
-          window.__map.on("idle", ()=> {
-          resolve(window.__map as Map);
-        })
-        } catch (e) {
-        console.error('Error getting map instance', e);
-        resolve(null)
-        }
-      }),
-      new Promise<Map | null>((resolve) => {
-        setTimeout(() => {
-        console.error('Map did not load in time');
-        resolve(null);
-        }, 10000);
-      })
-      ])
+        new Promise<Map | null>(async (resolve) => {
+          try {
+            window.__map.on("idle", () => {
+              resolve(window.__map as Map);
+            });
+          } catch (e) {
+            console.error("Error getting map instance", e);
+            resolve(null);
+          }
+        }),
+        new Promise<Map | null>((resolve) => {
+          setTimeout(() => {
+            console.error("Map did not load in time");
+            resolve(null);
+          }, 10000);
+        }),
+      ]);
     });
-  
+
     return {
       mapHandle,
-    }
-  } catch(e) {
+    };
+  } catch (e) {
     console.error(e);
-    const nullMap = await page.evaluateHandle(() => null)
+    const nullMap = await page.evaluateHandle(() => null);
     return {
       mapHandle: nullMap as JSHandle<Map | null>,
-    }
+    };
   }
 }
