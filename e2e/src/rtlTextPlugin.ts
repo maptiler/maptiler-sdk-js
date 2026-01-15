58894f11992b2eaee9e40e27c0593ae87022a05f
Les <iamiamiamles@gmail.com>
2026-01-15 14:34:26 +0000

    hotfix/rtl-text-plugin-is-not-called - ini rtl plugin on load

---
+++ e2e/src/rtlTextPlugin.ts
@@ -0,0 +1,52 @@
+import "../../dist/maptiler-sdk.css";
+import { Map as MapTiler, Language } from "../../src/index";
+
+async function main() {
+  const qp = new URLSearchParams(window.location.search);
+
+  const disableRTL = qp.get("disable-rtl") === "true" ? true : false;
+
+  const map = new MapTiler({
+    container: "map",
+    apiKey: "DOESNT_MATTER",
+    language: Language.ARABIC,
+    rtlTextPlugin: disableRTL ? false : undefined,
+    zoom: 7.5,
+    center: [43.648, 33.15],
+    terrain: false,
+  });
+
+  await map.onReadyAsync();
+
+  map.addSource("places-source", {
+    type: "geojson",
+    data: {
+      type: "FeatureCollection",
+      features: [
+        {
+          type: "Feature",
+          geometry: { type: "Point", coordinates: [43.648, 33.15] },
+          properties: { name: "!يلا حبيبي" }, // yla habibi
+        },
+      ],
+    },
+  });
+
+  map.addLayer({
+    id: "places-layer",
+    type: "symbol",
+    source: "places-source",
+    layout: {
+      "text-field": ["get", "name"],
+      "text-font": ["Noto Sans Regular"], // Ensure this font supports Arabic
+      "text-size": 100,
+    },
+    paint: {
+      "text-color": "red",
+    }
+  });
+
+  window.__map = map;
+}
+
+main();
