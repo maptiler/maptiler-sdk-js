53a5c4117c572ef168b927b9c977edcbf31df996
Les <iamiamiamles@gmail.com>
2026-01-22 12:50:52 +0000

    RD-1492 Update failing attempt to fix rtlText test in CI

--- e2e/tests/rtlTextPlugin.test.ts
+++ e2e/tests/rtlTextPlugin.test.ts
@@ -2,6 +2,11 @@ import { expect, test } from "@playwright/test";
 import loadFixtureAndGetMapHandle from "./helpers/loadFixtureAndGetMapHandle";
 
 test("Renders RTL text correctly when RTL plugin is enabled and incorrectly when disabled", async ({ page }) => {
+  await page.exposeFunction("notifyScreenshotStateReady", async (data: Record<string, TTestTransferData>) => {
+    const screenshotname = data.disabled ? "rtlTextPlugin-disabled.png" : "rtlTextPlugin-enabled.png";
+    await expect(page).toHaveScreenshot(screenshotname);
+  });
+
   await loadFixtureAndGetMapHandle({
     fixture: "rtlTextPlugin",
     page,
@@ -9,19 +14,9 @@ test("Renders RTL text correctly when RTL plugin is enabled and incorrectly when
 
   expect(await page.title()).toBe("MapTiler E2E RTL Text Plugin");
 
-  await page.exposeFunction("notifyScreenshotStateReady", async (data: Record<string, TTestTransferData>) => {
-    const screenshotname = data.disabled ? "rtlTextPlugin-disabled.png" : "rtlTextPlugin-enabled.png";
-    await expect(page).toHaveScreenshot(screenshotname);
-  });
-
   await loadFixtureAndGetMapHandle({
     fixture: "rtlTextPlugin",
     page,
     queryParams: { "disable-rtl": "true" },
-  });
-
-  await page.exposeFunction("notifyScreenshotStateReady", async (data: Record<string, TTestTransferData>) => {
-    const screenshotname = data.disabled ? "rtlTextPlugin-disabled.png" : "rtlTextPlugin-enabled.png";
-    await expect(page).toHaveScreenshot(screenshotname);
   });
 });
