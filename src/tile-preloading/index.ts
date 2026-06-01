9bec69a272e8e42f33d6739348aa654aa21a44ac
Les <iamiamiamles@gmail.com>
2026-06-01 16:22:42 +0100

    RD-1755 appease linter

--- src/tile-preloading/index.ts
+++ src/tile-preloading/index.ts
@@ -1,2 +1,11 @@
 export { TilePreloader } from "./TilePreloader";
-export type { CameraPosition, TilePreloadOptions, WithTilePreload, PreloadTilesForBoundsOptions, PreloadTilesForCameraPositionsOptions, PreloadTilesOptions, TilePreloadErrorCallback, TilePreloadProgressCallback } from "./types";
+export type {
+  CameraPosition,
+  TilePreloadOptions,
+  WithTilePreload,
+  PreloadTilesForBoundsOptions,
+  PreloadTilesForCameraPositionsOptions,
+  PreloadTilesOptions,
+  TilePreloadErrorCallback,
+  TilePreloadProgressCallback,
+} from "./types";
