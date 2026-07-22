abdecad482aba9af10172c7fab1a210d8cfa6d3b
Les <iamiamiamles@gmail.com>
2026-07-22 15:29:47 +0100

    hotfix/ml-version-bump-5.24 fix failing e2e tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -80,9 +80,9 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-    // command: 'npm run e2e:serve',
-    // url: 'http://localhost:5173',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: 'npm run e2e:serve',
+    url: 'http://localhost:5173',
+    reuseExistingServer: !process.env.CI,
+  },
 });
